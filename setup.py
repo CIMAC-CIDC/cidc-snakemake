d80b7547a5622db7ae935072370a2b9c67e67e38
Lloyd McCarthy <lloydmccar@gmail.com>
2019-02-11 11:55:39 -0500

    hardcoded snakemake version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ except ImportError:
 
 setup(
     name='snakemake',
-    version=versioneer.get_version(),
+    version='v5.3.1',
     cmdclass=versioneer.get_cmdclass(),
     author='Johannes Köster',
     author_email='johannes.koester@tu-dortmund.de',
